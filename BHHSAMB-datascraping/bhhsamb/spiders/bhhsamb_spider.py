1d5fec292a36a89b489cf968e0a7f28ef5ce119f
Anandu R Pillai <79455023+AnanduRPillai@users.noreply.github.com>
2024-07-26 09:43:39 +0530

    Update bhhsamb_spider.py

--- BHHSAMB-datascraping/bhhsamb/spiders/bhhsamb_spider.py
+++ BHHSAMB-datascraping/bhhsamb/spiders/bhhsamb_spider.py
@@ -1,89 +1,85 @@
 import scrapy
 import json
-import logging
-from scrapy.http import Request, FormRequest
-from scrapy.spiders import Spider
+from scrapy.http import Request
 
-headers = {
-    'Accept': '*/*',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    'Connection': 'keep-alive',
-    'Host': 'www.bhhsamb.com',
-    'Referer': 'https://www.bhhsamb.com/roster/Agents',
-    'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Linux"',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
-    'X-Requested-With': 'XMLHttpRequest'
-}
 
 class BhhsampSpider(scrapy.Spider):
-    name = "bhhsamp_"
-    allowed_domains = ["www.bhhsamb.com"]
-    start_urls = ['https://www.bhhsamb.com/CMS/CmsRoster/RosterSection?layoutID=963&pageSize=10&pageNumber=1&sortBy=random']
+    name = "bhhsamp"
+    allowed_domains = ["bhhsamb.com"]
+    start_urls = [
+        'https://www.bhhsamb.com/CMS/CmsRoster/RosterSection?layoutID=963&pageSize=10&pageNumber=1&sortBy=random'
+    ]
     agent_count = 0
     max_agents = 1120
+
+    headers = {
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        'Connection': 'keep-alive',
+        'Host': 'www.bhhsamb.com',
+        'Referer': 'https://www.bhhsamb.com/roster/Agents',
+        'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Linux"',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
 
     def start_requests(self):
         yield Request(
             url=self.start_urls[0],
-            headers=headers,
+            headers=self.headers,
             callback=self.parse,
             meta={'page': 1}
         )
 
     def parse(self, response):
         page = response.meta.get('page')
-        url_xpath = response.xpath('//a[@class="site-roster-card-image-link"]//@href').extract()
-        for p_url in url_xpath:
+        agent_links = response.xpath('//a[@class="site-roster-card-image-link"]//@href').extract()
+        for link in agent_links:
             if self.agent_count < self.max_agents:
-                product_url = 'https://www.bhhsamb.com' + p_url
-                yield Request(product_url, callback=self.parse_product)
+                agent_url = response.urljoin(link)
+                yield Request(agent_url, callback=self.parse_agent, headers=self.headers)
             else:
                 break
 
         page += 1
-        next_link = f'https://www.bhhsamb.com/CMS/CmsRoster/RosterSection?layoutID=963&pageSize=10&pageNumber={page}&sortBy=random'
+        next_page = f'https://www.bhhsamb.com/CMS/CmsRoster/RosterSection?layoutID=963&pageSize=10&pageNumber={page}&sortBy=random'
         if self.agent_count < self.max_agents:
-            yield Request(next_link, headers=headers, meta={'page': page}, callback=self.parse)
+            yield Request(next_page, headers=self.headers, meta={'page': page}, callback=self.parse)
 
-    def parse_product(self, response):
-        name = response.xpath('//p[@class="rng-agent-profile-contact-name"]/text()').extract_first('').strip()
-        image_url = response.xpath('//article[@class="rng-agent-profile-main"]//img//@src').get()
-        phone_number = response.xpath('//ul//li[@class="rng-agent-profile-contact-phone"]//a//text()').extract_first('').strip()
+    def parse_agent(self, response):
+        name = response.xpath('//p[@class="rng-agent-profile-contact-name"]/text()').get(default='').strip()
+        image_url = response.xpath('//article[@class="rng-agent-profile-main"]//img//@src').get(default='')
+        phone_number = response.xpath('//ul//li[@class="rng-agent-profile-contact-phone"]//a//text()').get(default='').strip()
 
-        address_xpath = response.xpath('//ul//li[@class="rng-agent-profile-contact-address"]//text()').extract()
-        address = ''.join([x.strip() for x in address_xpath if x.strip()])
+        address = ''.join(response.xpath('//ul//li[@class="rng-agent-profile-contact-address"]//text()').getall()).strip()
 
-        facebook = response.xpath('//li[@class="social-facebook"]//a/@href').get()
-        twitter = response.xpath('//li[@class="social-twitter"]//a/@href').get()
-        linkedin = response.xpath('//li[@class="social-linkedin"]//a/@href').get()
-        youtube = response.xpath('//li[@class="social-youtube"]//a/@href').get()
-        pinterest = response.xpath('//li[@class="social-pinterest"]//a/@href').get()
-        instagram = response.xpath('//li[@class="social-instagram"]//a/@href').get()
+        social_links = {
+            'facebook': response.xpath('//li[@class="social-facebook"]//a/@href').get(),
+            'twitter': response.xpath('//li[@class="social-twitter"]//a/@href').get(),
+            'linkedin': response.xpath('//li[@class="social-linkedin"]//a/@href').get(),
+            'youtube': response.xpath('//li[@class="social-youtube"]//a/@href').get(),
+            'pinterest': response.xpath('//li[@class="social-pinterest"]//a/@href').get(),
+            'instagram': response.xpath('//li[@class="social-instagram"]//a/@href').get(),
+        }
 
         agent_data = {
             'name': name,
             'image_url': image_url,
             'phone_number': phone_number,
             'address': address,
-            'description': None,
-            'Facebook': facebook if facebook else None,
-            'twitter': twitter if twitter else None,
-            'linkedin': linkedin if linkedin else None,
-            'youtube': youtube if youtube else None,
-            'pinterest': pinterest if pinterest else None,
-            'instagram': instagram if instagram else None
+            'description': response.xpath('//article[@class="rng-agent-profile-content"]//span/text()').get(default='').strip(),
+            'social_links': {key: value for key, value in social_links.items() if value}
         }
 
-        with open('agents_data.json', 'a') as file:
+        with open('data.json', 'a') as file:
             file.write(json.dumps(agent_data, separators=(',', ':')) + '\n')
 
         self.agent_count += 1
         if self.agent_count >= self.max_agents:
             self.crawler.engine.close_spider(self, 'Reached the target number of agents')
-
